fa808c47c4bac524412a0c272fa6e08127f6a95f
Béla Papp <bela.papp@outlook.hu>
2016-11-19 15:38:09 +0100

    phpMyAdmin

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('phpMyAdmin');
+
 Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index']);
 
 Auth::routes();
