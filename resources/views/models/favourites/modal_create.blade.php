eb297a0abecbb07d0f90319ffe0fc77953c40896
Béla Papp <bela.papp@outlook.hu>
2017-01-10 07:19:20 +0100

    x

--- resources/views/models/favourites/modal_create.blade.php
+++ resources/views/models/favourites/modal_create.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="createFavourites" tabindex="-1" role="dialog" aria-labelledby="createFavouritesModalLabel" aria-hidden="true">
-  <div class="modal-dialog  modal-lg" role="document">
+  <div class="modal-dialog  modal" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
--- resources/views/models/favourites/modal_delete.blade.php
+++ resources/views/models/favourites/modal_delete.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="deleteFavourites" tabindex="-1" role="dialog" aria-labelledby="deleteFavouritesModalLabel" aria-hidden="true">
-  <div class="modal-dialog  modal-lg" role="document">
+  <div class="modal-dialog  modal" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
