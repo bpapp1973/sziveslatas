4290bdb1c0762b9392c1399d7bcab9d6e86aa124
Béla Papp <bela.papp@outlook.hu>
2017-01-10 21:38:50 +0100

    drp+fullcalendar

--- resources/views/models/orders/modal_fields.blade.php
+++ resources/views/models/orders/modal_fields.blade.php
@@ -1,11 +1,13 @@
 <!-- Companies Id Field -->
-<div class="form-group{{ $errors->has('companies_id') ? ' has-error' : '' }}">
-	{!! Form::label('companies_id', 'Companies Id', ['class' => 'col-md-4 control-label']) !!}
+{!! Form::hidden('companies_id', $ads->companies_id, ['id' => 'companies_id']) !!}
+
+<div class="form-group{{ $errors->has('company') ? ' has-error' : '' }}">
+	{!! Form::label('company', 'Hirdető', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-    	{!! Form::number('companies_id', null, ['class' => 'form-control']) !!}
-        @if ($errors->has('companies_id'))
+    	{!! Form::text('company', $ads->company->name, ['class' => 'form-control']) !!}
+        @if ($errors->has('company'))
             <span class="help-block">
-                <strong>{{ $errors->first('companies_id') }}</strong>
+                <strong>{{ $errors->first('company') }}</strong>
             </span>
         @endif
     </div>
@@ -13,27 +15,15 @@
 
 
 <!-- Users Id Field -->
-<div class="form-group{{ $errors->has('users_id') ? ' has-error' : '' }}">
-	{!! Form::label('users_id', 'Users Id', ['class' => 'col-md-4 control-label']) !!}
-    <div class="col-md-6">
-    	{!! Form::number('users_id', null, ['class' => 'form-control']) !!}
-        @if ($errors->has('users_id'))
-            <span class="help-block">
-                <strong>{{ $errors->first('users_id') }}</strong>
-            </span>
-        @endif
-    </div>
-</div>
+{!! Form::hidden('users_id', Auth::user()->id, ['id' => 'users_id']) !!}
 
-
-<!-- First Name Field -->
-<div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
-    {!! Form::label('first_name', 'First Name', ['class' => 'col-md-4 control-label']) !!}
+<div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+	{!! Form::label('username', 'Felhasználó', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-    	{!! Form::text('first_name', null, ['class' => 'form-control']) !!}
-        @if ($errors->has('first_name'))
+    	{!! Form::text('username', Auth::user()->username, ['class' => 'form-control']) !!}
+        @if ($errors->has('username'))
             <span class="help-block">
-                <strong>{{ $errors->first('first_name') }}</strong>
+                <strong>{{ $errors->first('username') }}</strong>
             </span>
         @endif
     </div>
@@ -42,12 +32,26 @@
 
 <!-- Last Name Field -->
 <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
-    {!! Form::label('last_name', 'Last Name', ['class' => 'col-md-4 control-label']) !!}
+    {!! Form::label('last_name', 'Vezetéknév', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-    	{!! Form::text('last_name', null, ['class' => 'form-control']) !!}
+    	{!! Form::text('last_name', Auth::user()->last_name, ['class' => 'form-control']) !!}
         @if ($errors->has('last_name'))
             <span class="help-block">
                 <strong>{{ $errors->first('last_name') }}</strong>
+            </span>
+        @endif
+    </div>
+</div>
+
+
+<!-- First Name Field -->
+<div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
+    {!! Form::label('first_name', 'Keresztnév', ['class' => 'col-md-4 control-label']) !!}
+    <div class="col-md-6">
+        {!! Form::text('first_name', Auth::user()->first_name, ['class' => 'form-control']) !!}
+        @if ($errors->has('first_name'))
+            <span class="help-block">
+                <strong>{{ $errors->first('first_name') }}</strong>
             </span>
         @endif
     </div>
@@ -58,7 +62,7 @@
 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
     {!! Form::label('email', 'Email', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-        {!! Form::email('email', null, ['class' => 'form-control']) !!}
+        {!! Form::email('email', Auth::user()->email, ['class' => 'form-control']) !!}
         @if ($errors->has('email'))
             <span class="help-block">
                 <strong>{{ $errors->first('email') }}</strong>
@@ -70,9 +74,9 @@
 
 <!-- Phone Field -->
 <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-    {!! Form::label('phone', 'Phone', ['class' => 'col-md-4 control-label']) !!}
+    {!! Form::label('phone', 'Telefonszám', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-    	{!! Form::text('phone', null, ['class' => 'form-control']) !!}
+    	{!! Form::text('phone', Auth::user()->phone, ['class' => 'form-control']) !!}
         @if ($errors->has('phone'))
             <span class="help-block">
                 <strong>{{ $errors->first('phone') }}</strong>
@@ -81,9 +85,3 @@
     </div>
 </div>
 
-
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Mentés', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('orders.index') !!}" class="nav-link btn btn-warning-outline btn-warning">Mégsem</a>
-</div>
