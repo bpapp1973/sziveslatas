ea397aca5b36522a678cb6920282f691c813d40a
Béla Papp <bela.papp@outlook.hu>
2017-01-22 08:26:10 +0100

    fix nullpointer crash in SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -63,7 +63,7 @@ class SearchController extends Controller
     {
         $input = $request->all();
         $query = [];
-        $datequery = [];
+        $textsearch="";
 
         if(isset($input['category']) && $input['category']!="") {
             array_push($query, ["categories_id","=",$input['category']]);
@@ -72,23 +72,26 @@ class SearchController extends Controller
             array_push($query, ['startdate','<=',$input['when']]);
             array_push($query, ['enddate','>=',$input['when']]);
         }
+        if(isset($input['textsearch']) && $input['textsearch']!="") {
+            $textsearch=$input['when'];
+        }
         //http://laravel.io/forum/09-18-2014-orm-query-where-clause-on-related-table
         //https://github.com/jarektkaczyk/eloquence
         $ads = Ads::with('city','category','tag', 'company')
                 ->where($query)
-                ->where(function ($query) use ($input) {
-                    $query->where("title","LIKE","%".$input['textsearch']."%")
-                          ->orWhere("description","LIKE","%".$input['textsearch']."%")
-                          ->orWhere(function($q) use ($input) {
-                                $q->whereHas('tag', function($cq) use ($input) {
-                                    $cq->where([['name', $input["textsearch"]],
+                ->where(function ($query) use ($textsearch) {
+                    $query->where("title","LIKE","%".$textsearch."%")
+                          ->orWhere("description","LIKE","%".$textsearch."%")
+                          ->orWhere(function($q) use ($textsearch) {
+                                $q->whereHas('tag', function($cq) use ($textsearch) {
+                                    $cq->where([['name', $textsearch],
                                                 ['container_type', 'ad']
                                         ]);
                                 });
                           })
-                          ->orWhere(function($q) use ($input) {
-                                $q->whereHas('company', function($cq) use ($input) {
-                                    $cq->where('name',"LIKE","%".$input['textsearch']."%");
+                          ->orWhere(function($q) use ($textsearch) {
+                                $q->whereHas('company', function($cq) use ($textsearch) {
+                                    $cq->where('name',"LIKE","%".$textsearch."%");
                                 });
                           })
                           ;
