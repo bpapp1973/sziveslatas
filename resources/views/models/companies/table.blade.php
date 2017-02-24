cb77393526853109f395c4048090108f904b6efb
bpapp1973 <bela.papp@outlook.hu>
2017-02-24 13:41:55 +0100

    companies
    fix links issue

--- resources/views/models/companies/table.blade.php
+++ resources/views/models/companies/table.blade.php
@@ -9,20 +9,20 @@
         <th colspan="3">Művelet</th>
     </thead>
     <tbody>
-    @foreach($companies as $companies)
+    @foreach($companies as $company)
         <tr>
-            <td>{!! $companies->name !!}</td>
-            <td>{!! $companies->title !!}</td>
-            <td>{!! $companies->category->name !!}</td>
-            <td>{!! $companies->city->name !!}</td>
-            <td>{!! $companies->address !!}</td>
-            <td>{!! $companies->website !!}</td>
+            <td>{!! $company->name !!}</td>
+            <td>{!! $company->title !!}</td>
+            <td>{!! $company->category->name !!}</td>
+            <td>{!! $company->city->name !!}</td>
+            <td>{!! $company->address !!}</td>
+            <td>{!! $company->website !!}</td>
             <td>
-                {!! Form::open(['route' => ['companies.destroy', $companies->id], 'method' => 'delete']) !!}
+                {!! Form::open(['route' => ['companies.destroy', $company->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('companies.show', [$companies->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                    <a href="{!! route('companies.show', [$company->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                     @if(Auth::user())
-                    <a href="{!! route('companies.edit', [$companies->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                    <a href="{!! route('companies.edit', [$company->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Biztos vagy benne?')"]) !!}
                     @endif
                 </div>
